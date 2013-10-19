5044d48a1896115973f0ed85c6d305c8cd939e00
Eric Gonzalez <eric@bypassmobile.com>
2013-10-19 17:52:31 -0500

    Extracted report logic out of the uploader.

---
+++ CriticismProject/criticism/src/main/java/com/ericrgon/criticism/ReportGenerator.java
@@ -0,0 +1,82 @@
+package com.ericrgon.criticism;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ReportGenerator {
+
+
+
+    private static final String TAG_REGEX = ".*\\{[^)]*\\}.*";
+    private static final Pattern TAG_PATTERN = Pattern.compile(TAG_REGEX);
+
+    private static final String APP_TAG = "{app_name}";
+    private static final String VERSION_TAG = "{version}";
+    private static final String DESCRIPTION_TAG = "{description}";
+    private static final String LOGS_TAG = "{logs}";
+
+    private final String applicationName;
+    private final String applicationVersion;
+    private final String description;
+    private final File cacheDir;
+
+    public ReportGenerator(String applicationName, String applicationVersion, String description, File cacheDir) {
+        this.applicationName = applicationName;
+        this.applicationVersion = applicationVersion;
+        this.description = description;
+        this.cacheDir = cacheDir;
+    }
+
+    public File generate(Context context) throws IOException {
+        InputStream reportIndex = context.getResources().openRawResource(R.raw.index);
+        return writeReport(reportIndex,cacheDir);
+    }
+
+    private File writeReport(InputStream sourceStream,File cacheDir) throws IOException {
+        File cachedIdex = cacheDir.createTempFile("index",".html");
+        FileOutputStream fileOutputStream = new FileOutputStream(cachedIdex);
+
+        InputStreamReader inputStreamReader = new InputStreamReader(sourceStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        OutputStreamWriter stringWriter = new OutputStreamWriter(fileOutputStream);
+
+        String current;
+        while ((current = bufferedReader.readLine()) != null){
+            //Check if the current line contains a tag.
+            if(current.matches(TAG_REGEX)){
+                Matcher matcher = TAG_PATTERN.matcher(current);
+                while (matcher.find()){
+                    String tag = matcher.group(0);
+                    if (tag.contains(APP_TAG)) {
+                        current = current.replace(APP_TAG,applicationName);
+
+                    } else if(tag.contains(VERSION_TAG)){
+                        current = current.replace(VERSION_TAG,applicationVersion);
+                    } else if(tag.contains(DESCRIPTION_TAG)){
+                        current = current.replace(DESCRIPTION_TAG,description);
+                    } else if(tag.contains(LOGS_TAG)){
+                        String logsString = Logs.getLogString();
+                        current = current.replace(LOGS_TAG,logsString);
+                    }
+
+                }
+            }
+            stringWriter.write(current + "\n");
+        }
+
+        stringWriter.close();
+
+        return cachedIdex;
+    }
+}
--- CriticismProject/criticism/src/main/java/com/ericrgon/criticism/S3Uploader.java
+++ CriticismProject/criticism/src/main/java/com/ericrgon/criticism/S3Uploader.java
@@ -13,19 +13,13 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.Permission;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class S3Uploader extends AsyncTask<Void,Void,Void>{
@@ -46,14 +40,6 @@ public class S3Uploader extends AsyncTask<Void,Void,Void>{
     private static final Grant GRANT = new Grant(GroupGrantee.AllUsers, Permission.FullControl);
     private static final AccessControlList ACCESS_CONTROL_LIST = new AccessControlList();
 
-    private static final String TAG_REGEX = ".*\\{[^)]*\\}.*";
-    private static final Pattern TAG_PATTERN = Pattern.compile(TAG_REGEX);
-
-    private static final String APP_TAG = "{app_name}";
-    private static final String VERSION_TAG = "{version}";
-    private static final String DESCRIPTION_TAG = "{description}";
-    private static final String LOGS_TAG = "{logs}";
-
     public S3Uploader(Context context,String bucketName) {
         this.context = context;
         this.bucketName = bucketName;
@@ -71,19 +57,6 @@ public class S3Uploader extends AsyncTask<Void,Void,Void>{
         ACCESS_CONTROL_LIST.grantAllPermissions(GRANT);
     }
 
-    /**
-     * Report name is structured as com.example-hh:mm-dd-mm-yyyy
-     *
-     * @param context
-     * @return
-     */
-    private String generateReportName(Context context) {
-        String packageName = context.getPackageName();
-        Date currentDate = new Date();
-        SimpleDateFormat formattedDate = new SimpleDateFormat("HH:mm-dd-MM-yyyy");
-        return packageName + "-" + formattedDate.format(currentDate);
-    }
-
     public void upload(){
         new Thread(){
             @Override
@@ -97,10 +70,11 @@ public class S3Uploader extends AsyncTask<Void,Void,Void>{
                 putObjectRequest.setAccessControlList(ACCESS_CONTROL_LIST);
                 amazonS3Client.putObject(putObjectRequest);
 
+                ReportGenerator reportGenerator = new ReportGenerator(applicationName,applicationVersion,description,cacheDir);
+
                 File report = null;
                 try {
-                    InputStream reportIndex = context.getResources().openRawResource(R.raw.index);
-                    report = writeReport(reportIndex, cacheDir);
+                    report = reportGenerator.generate(context);
 
                     //Upload report
                     PutObjectRequest index = new PutObjectRequest(getPath(),"index.html",report);
@@ -137,73 +111,42 @@ public class S3Uploader extends AsyncTask<Void,Void,Void>{
         return bucketName + "/" + reportName;
     }
 
-    private File writeReport(InputStream sourceStream,File cacheDir) throws IOException {
-        File cachedIdex = cacheDir.createTempFile("index",".html");
-        FileOutputStream fileOutputStream = new FileOutputStream(cachedIdex);
-
-        InputStreamReader inputStreamReader = new InputStreamReader(sourceStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-        OutputStreamWriter stringWriter = new OutputStreamWriter(fileOutputStream);
-
-        String current;
-        while ((current = bufferedReader.readLine()) != null){
-            //Check if the current line contains a tag.
-            if(current.matches(TAG_REGEX)){
-                Matcher matcher = TAG_PATTERN.matcher(current);
-                while (matcher.find()){
-                    String tag = matcher.group(0);
-                    if (tag.contains(APP_TAG)) {
-                        current = current.replace(APP_TAG,applicationName);
-
-                    } else if(tag.contains(VERSION_TAG)){
-                        current = current.replace(VERSION_TAG,applicationVersion);
-                    } else if(tag.contains(DESCRIPTION_TAG)){
-                        current = current.replace(DESCRIPTION_TAG,description);
-                    } else if(tag.contains(LOGS_TAG)){
-                        String logsString = Logs.getLogString();
-                        current = current.replace(LOGS_TAG,logsString);
-                    }
-
-                }
-            }
-            stringWriter.write(current + "\n");
-        }
-
-        stringWriter.close();
-
-        return cachedIdex;
+    /**
+     * Report name is structured as com.example-hh:mm-dd-mm-yyyy
+     *
+     * @param context
+     * @return
+     */
+    private String generateReportName(Context context) {
+        String packageName = context.getPackageName();
+        Date currentDate = new Date();
+        SimpleDateFormat formattedDate = new SimpleDateFormat("HH:mm-dd-MM-yyyy");
+        return packageName + "-" + formattedDate.format(currentDate);
     }
 
-    public String getDescription() {
-        return description;
+    @Override
+    protected Void doInBackground(Void... voids) {
+        upload();
+        return null;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public boolean isSendLogs() {
-        return sendLogs;
-    }
-
     public void setSendLogs(boolean sendLogs) {
         this.sendLogs = sendLogs;
     }
 
-    public byte[] getScreenshot() {
-        return screenshot;
+    public boolean isSendLogs() {
+        return sendLogs;
     }
 
     public void setScreenshot(byte[] screenshot) {
         this.screenshot = screenshot;
     }
 
-    @Override
-    protected Void doInBackground(Void... voids) {
-
-        upload();
-
-        return null;
+    public byte[] getScreenshot() {
+        return screenshot;
     }
 }
